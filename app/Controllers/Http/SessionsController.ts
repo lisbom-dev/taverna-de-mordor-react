147fdcbb09e56a07b2941d6331785d11f9563c6d
Lucas-Marques-Leao <lucasmarquesleao@outlook.com>
2022-07-03 01:00:52 -0300

    Create README.md

--- app/Controllers/Http/SessionsController.ts
+++
@@ -1,8 +0,0 @@
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
-export default class SessionsController {
-  public async destroy({ auth, response }: HttpContextContract) {
-    await auth.logout()
-    return response.redirect('/')
-  }
-}
--- app/Policies/EventPolicy.ts
+++
@@ -1,8 +0,0 @@
-import { BasePolicy } from '@ioc:Adonis/Addons/Bouncer'
-import User from 'App/Models/User'
-
-export default class EventPolicy extends BasePolicy {
-  public async invoke(user: User) {
-    return user.isAdm
-  }
-}
