1cef4f862a1d3af664fbd1e5b9ea34c236187090
Lucas-Marques-Leao <lucasmarquesleao@outlook.com>
2022-07-08 13:27:22 -0300

    fix: folders

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
-import SignIn from "../resources/SignIn";
-import SignUp from "../resources/views/users/SignUp";
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
 
 const AuthRoutes: React.FC = () => {
   const { signed } = useAuth();
@@ -9,20 +9,24 @@ const AuthRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/sign-in" />} />
-      {signed ? <Route path="/home" element={<></>} /> : <Navigate to={"/sign-in"} />}
+      {signed ? (
+        <Route path="/home" element={<></>} />
+      ) : (
+        <Navigate to={"/sign-in"} />
+      )}
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/logout" element={<></>}/>
-      <Route path="/users" element={<></>}/>
-      <Route path="/users/:id" element={<></>}/>
-      <Route path="/adms/:id" element={<></>}/>
-      <Route path="/masters/:id" element={<></>}/>
-      <Route path="/events/:event_id/boards" element={<></>}/>
-      <Route path="/boards/:board_id/chat" element={<></>}/>   
-      <Route path="/boards/:board_id/players" element={<></>}/>
-      <Route path="/boards/:board_id/chat" element={<></>}/>
-      <Route path="/notifications" element={<></>}/>
-      <Route path="/notifications/:id" element={<></>}/>     
+      <Route path="/logout" element={<></>} />
+      <Route path="/users" element={<></>} />
+      <Route path="/users/:id" element={<></>} />
+      <Route path="/adms/:id" element={<></>} />
+      <Route path="/masters/:id" element={<></>} />
+      <Route path="/events/:event_id/boards" element={<></>} />
+      <Route path="/boards/:board_id/chat" element={<></>} />
+      <Route path="/boards/:board_id/players" element={<></>} />
+      <Route path="/boards/:board_id/chat" element={<></>} />
+      <Route path="/notifications" element={<></>} />
+      <Route path="/notifications/:id" element={<></>} />
     </Routes>
   );
 };
