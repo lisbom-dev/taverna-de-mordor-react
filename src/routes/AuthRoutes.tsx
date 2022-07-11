a60d888ca7a6e0cb00fe25e0c28cb2aef1f8dc5e
zogss <yanlucasbranco@gmail.com>
2022-07-11 00:03:47 -0300

    feat: change path and add new components in element

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+import Home from "../resources/views/Home";
 import SignIn from "../resources/views/SignIn";
+import Team from "../resources/views/Team";
 import List from "../resources/views/users/List";
 import SignUp from "../resources/views/users/SignUp";
 
@@ -13,17 +15,19 @@ const AuthRoutes: React.FC = () => {
       <Route path="/home" element={<Home />} />
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/logout" element={<></>}/>
-      <Route path="/users" element={<></>}/>
-      <Route path="/users/:id" element={<></>}/>
-      <Route path="/adms/:id" element={<></>}/>
-      <Route path="/masters/:id" element={<></>}/>
-      <Route path="/events/:event_id/boards" element={<></>}/>
-      <Route path="/boards/:board_id/chat" element={<></>}/>   
-      <Route path="/boards/:board_id/players" element={<></>}/>
-      <Route path="/boards/:board_id/chat" element={<></>}/>
-      <Route path="/notifications" element={<></>}/>
-      <Route path="/notifications/:id" element={<></>}/>     
+      <Route path="/logout" element={<></>} />
+      <Route path="/equipe" element={<Team />} />
+      <Route path="/users" element={<List />} />
+      <Route path="/users/:id" element={<></>} />
+      <Route path="/adms/:id" element={<></>} />
+      <Route path="/masters/:id" element={<></>} />
+      <Route path="/events" element={<></>} />
+      <Route path="/events/:event_id/boards" element={<></>} />
+      <Route path="/boards/:board_id/chat" element={<></>} />
+      <Route path="/boards/:board_id/players" element={<></>} />
+      <Route path="/boards/:board_id/chat" element={<></>} />
+      <Route path="/notifications" element={<></>} />
+      <Route path="/notifications/:id" element={<></>} />
     </Routes>
   );
 };
