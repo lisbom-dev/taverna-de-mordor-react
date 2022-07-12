ac9f907b2bdb20a8c2319a56851691c5b0761d8a
Lucas-Marques-Leao <lucasmarquesleao@outlook.com>
2022-07-12 02:46:38 -0300

    fix: Interfaces correct importing

--- src/shared/interfaces/IBoard.ts
+++ src/shared/interfaces/IBoard.ts
@@ -2,21 +2,21 @@ import { DateTime } from "luxon";
 import IEvent from "./IEvent";
 import IReviews from "./IReviews";
 import ISystem from "./ISystem";
-import IUserCardProps from "./IUserCardProps";
+import IUser from "./IUser";
 
 export default interface IBoard {
-    id: number,
-    name: number,
-    systemId: number,
-    maxPlayers: number,
-    masterId: number,
-    master: IUserCardProps,
-    system: ISystem [],
-    reviews: IReviews [],
-    events: IEvent [],
-    players: IUserCardProps [],
-    avaluation: number,
-    currentSection: number,
-    createdAt: DateTime,
-    updatedAt: DateTime
+  id: number;
+  name: number;
+  systemId: number;
+  maxPlayers: number;
+  masterId: number;
+  master: IUser;
+  system: ISystem[];
+  reviews: IReviews[];
+  events: IEvent[];
+  players: IUser[];
+  avaluation: number;
+  currentSection: number;
+  createdAt: DateTime;
+  updatedAt: DateTime;
 }
--- src/shared/interfaces/IReviews.ts
+++ src/shared/interfaces/IReviews.ts
@@ -1,17 +1,17 @@
 import { DateTime } from "luxon";
 import IBoard from "./IBoard";
 import IEvent from "./IEvent";
-import IUserCardProps from "./IUserCardProps";
+import IUser from "./IUser";
 
 export default interface IReviews {
-    id: number,
-    rating: number,
-    comment: string,
-    senderId: number,
-    sender: IUserCardProps,
-    board: IBoard [],
-    event: IEvent [],
-    master: IUserCardProps [],
-    createdAt: DateTime,
-    updatedAt: DateTime
+  id: number;
+  rating: number;
+  comment: string;
+  senderId: number;
+  sender: IUser;
+  board: IBoard[];
+  event: IEvent[];
+  master: IUser[];
+  createdAt: DateTime;
+  updatedAt: DateTime;
 }
